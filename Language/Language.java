3270d7f445851c30e339556652034b86cd880944
Leclouerec <leclouerec@gmx.fr>
2017-01-22 01:59:05 +0100

    Fichier de langue
    Ajout d'un fichier pour la gestion des langues

---
+++ Language/Language.java
@@ -0,0 +1,18 @@
+package com.mp.SimpleChat.Language;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * Classe abstraite permettant la gestion des langues
+ *
+ */
+
+public class Language {
+	
+}
